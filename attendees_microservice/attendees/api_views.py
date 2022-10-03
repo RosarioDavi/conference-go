6317a82eb4e461a677d2c7845c39448a9e881357
Rosario <rosariodavi9@gmail.com>
2022-10-03 18:07:42 -0400

    syncing data

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,15 +1,19 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee
-from events.models import Conference
+from .models import Attendee, ConferenceVO
 from django.views.decorators.http import require_http_methods
 import json
 
 
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
+
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeesListEncoder,
@@ -19,9 +23,10 @@ def api_list_attendees(request, conference_id):
 
         # Get the Conference object and put it in the content dict
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f"/api/conference/{conference_vo_id}/"
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
@@ -64,9 +69,9 @@ def api_list_attendees(request, conference_id):
     # return JsonResponse({"attendee": response})
 
 
-class ConferenceListEncoder(ModelEncoder):
-    model = Conference
-    properties = ["name"]
+# class ConferenceListEncoder(ModelEncoder):
+#     model = Conference
+#     properties = ["name"]
 
 
 class AttendeesListEncoder(ModelEncoder):
@@ -83,7 +88,7 @@ class AttendeeDetailEncoder(ModelEncoder):
         "created",
         "conference",
     ]
-    encoders = {"conference": ConferenceListEncoder()}
+    encoders = {"conference": ConferenceVODetailEncoder()}
 
 
 @require_http_methods(["DELETE", "GET", "PUT"])
