49c67a255e63cfaddebfbda1d579fdeca6397b9f
Rosario <rosariodavi9@gmail.com>
2022-10-06 12:15:41 -0400

    added pub/sub

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee, ConferenceVO
+from .models import Attendee, ConferenceVO, AccountVO
 from django.views.decorators.http import require_http_methods
 import json
 
@@ -9,55 +9,6 @@ class ConferenceVODetailEncoder(ModelEncoder):
     model = ConferenceVO
     properties = ["name", "import_href"]
 
-
-@require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_vo_id=None):
-    if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_vo_id)
-        return JsonResponse(
-            {"attendees": attendees},
-            encoder=AttendeesListEncoder,
-        )
-    else:
-        content = json.loads(request.body)
-
-        # Get the Conference object and put it in the content dict
-        try:
-            conference_href = f"/api/conference/{conference_vo_id}/"
-            conference = ConferenceVO.objects.get(import_href=conference_href)
-            content["conference"] = conference
-        except ConferenceVO.DoesNotExist:
-            return JsonResponse(
-                {"message": "Invalid conference id"},
-                status=400,
-            )
-
-        attendee = Attendee.objects.create(**content)
-        return JsonResponse(
-            attendee,
-            encoder=AttendeeDetailEncoder,
-            safe=False,
-        )
-
-    """
-    Lists the attendees names and the link to the attendee
-    for the specified conference id.
-
-    Returns a dictionary with a single key "attendees" which
-    is a list of attendee names and URLS. Each entry in the list
-    is a dictionary that contains the name of the attendee and
-    the link to the attendee's information.
-
-    {
-        "attendees": [
-            {
-                "name": attendee's name,
-                "href": URL to the attendee,
-            },
-            ...
-        ]
-    }
-    """
     # for attendee in attendees:
     #     response.append(
     #         {
@@ -89,6 +40,17 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {"conference": ConferenceVODetailEncoder()}
+
+    def get_extra_data(self, o):
+        # Get the count of AccountVO objects with email equal to o.email
+        count = AccountVO.objects.filter(email=o.email).count()
+
+        # Return a dictionary with "has_account": True if count > 0
+        if count > 0:
+            return {"has_account": True}
+            # Otherwise, return a dictionary with "has_account": False
+        else:
+            return {"has_account": False}
 
 
 @require_http_methods(["DELETE", "GET", "PUT"])
@@ -140,3 +102,52 @@ def api_show_attendee(request, pk):
     #         "href": attendee.get_api_url(),
     #     },
     # }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_attendees(request, conference_vo_id=None):
+    if request.method == "GET":
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
+        return JsonResponse(
+            {"attendees": attendees},
+            encoder=AttendeesListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        # Get the Conference object and put it in the content dict
+        try:
+            conference_href = f"/api/conferences/{conference_vo_id}/"
+            conference = ConferenceVO.objects.get(import_href=conference_href)
+            content["conference"] = conference
+        except ConferenceVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"},
+                status=400,
+            )
+
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
+    """
+    Lists the attendees names and the link to the attendee
+    for the specified conference id.
+
+    Returns a dictionary with a single key "attendees" which
+    is a list of attendee names and URLS. Each entry in the list
+    is a dictionary that contains the name of the attendee and
+    the link to the attendee's information.
+
+    {
+        "attendees": [
+            {
+                "name": attendee's name,
+                "href": URL to the attendee,
+            },
+            ...
+        ]
+    }
+    """
